7cbbe330f10359186913387f3549ac58f3f1195a
naota7118 <naota7118@gmail.com>
2024-12-02 10:23:44 +0900

    employeesコントローラのルーティングを設定

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,9 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
-  # Defines the root path route ("/")
-  # root "posts#index"
+  namespace :api, { format: 'json'} do
+    namespace :v1 do
+      resources :employees, only: [:index, :show]
+    end
+  end
 end
